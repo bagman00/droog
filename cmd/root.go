99af60a784433db26220ac53f1f5b0ffc3ffe360
bagman00 <bhandyuvaneta@gmail.com>
2026-07-04 15:49:33 +0530

    wire up cobra root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,19 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "droog",
+	Short: "watch anything together, no servers, no accounts",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("droog is alive")
+	},
+}
 
 func Execute() {
-	fmt.Println("droog is alive")
+	rootCmd.Execute()
 }
